008004c5703026c100c325c3f565eac19d1cd9e9
Elst21 <grippa30rus@gmail.com>
2025-04-16 11:32:07 +0200

    Обновил README с описанием проекта

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+alert("Hallo, Elena!");
+const age = 35; // например, ваш возраст
+    const name = "Elena"; // ваше имя
+    const city = "Willingen"; // ваш город
+    console.log("35:", age);
+    console.log("Elena:", name);
+    console.log("Willingen:", city);
+    
+let x = 40;
+let y = 2;
 
+console.log(x + y); 
+console.log(x - y); 
+console.log(x * y); 
+console.log(x / y); 
+console.log(x % y); 
+
+console.log("3" + 2); 
+console.log(3 + "2"); 
+
+console.log("40" - 2); 
+console.log("40" * 2); 
+console.log("40" / 2); 
+
+console.log(Number("20") + 5); 
+     
